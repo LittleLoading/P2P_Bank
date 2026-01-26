143990b85036cd5a7095b63b6ae6e745e928b0f7
Niko2357 <niki.polach@gmail.com>
2026-01-26 21:29:20 +0100

    class for handling responses, success or error

---
+++ Response.py
@@ -0,0 +1,21 @@
+class Response:
+    @staticmethod
+    def success(command, message=""):
+        """
+        Returns success message of some process.
+        :param command: Command that was successful.
+        :param message: Message of success, unique for each command.
+        :return: string
+        """
+        messg = f"{command.upper()} {message}".strip()
+        return f"{messg}\n"
+
+    @staticmethod
+    def error(message):
+        """
+        Return error messages.
+        :param message: string error message
+        :return: string
+        """
+        return f"{message}\n"
+
