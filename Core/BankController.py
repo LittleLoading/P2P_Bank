4e1a1982535d87fdfb8182ee8775890d6cb51126
Niko2357 <niki.polach@gmail.com>
2026-01-27 20:36:06 +0100

    some cosmetic and format edits

--- Core/BankController.py
+++ Core/BankController.py
@@ -7,26 +7,19 @@ class BankController:
         self.config = config
         self.db = db
 
-    def proccess_command(self, raw_data):
+    def process_command(self, raw_data):
         if not raw_data:
             return ""
-
         parts = raw_data.strip().split()
         if not parts:
             return Response.error("ER Unknown command")
-
         cmd_code = parts[0].upper()
-
         try:
             if cmd_code == "BC":
                 return BankCode.execute(self.config)
-
             elif cmd_code == "AC":
                 return AccountCreate.execute(self.config, self.db)
-
-
             else:
                 return Response.error(f"ER Command {cmd_code} not found.")
-
         except Exception as e:
             return Response.error(f"ERROR: {str(e)}")
