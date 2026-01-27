f3598a0526bf35ca049c28936324d7dc6292b6dc
Niko2357 <niki.polach@gmail.com>
2026-01-27 20:29:13 +0100

    changed language to english

--- Commands/BankCode.py
+++ Commands/BankCode.py
@@ -9,4 +9,4 @@ def execute(config):
         ip_address = config.get("host", "127.0.0.1")
         return Response.success("BC", ip_address)
     except Exception as e:
-        return Response.error(f"ER Chyba pri ziskavani kodu banky: {str(e)}")
+        return Response.error(f"ER Error getting bank code: {str(e)}")
